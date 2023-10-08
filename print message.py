707054d2ecc3915d1926cb2145a7bbb6c844011f
Thanveertt <thanveer7tt@gmail.com>
2023-10-08 23:38:52 +0530

    Create print message.py

---
+++ print message.py
@@ -0,0 +1,10 @@
+message ="Manchester United"
+print(message)
+message =123
+print(message)
+message ='t'
+print(message)
+message=20.50
+print(message)
+print("message is %0.5f"%message)
+print("message is %d"%message)
